0ae45e7e66a83c6c8ca94e44307729dec7132bdf
trumanng10 <35524030+trumanng10@users.noreply.github.com>
2024-12-18 11:48:09 +0800

    Create Lab5API.py

---
+++ Day1/Lab5API.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    sender_ip = request.remote_addr  # Captures the IP of the requester
+    cookies = request.cookies  # Retrieves cookies sent with the request
+    return jsonify(
+        message="Hello, Flask running inside Docker!",
+        SenderIP=sender_ip,
+        HeaderFormat="JSON",
+        Cookies=cookies  # Include cookies in the response
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
